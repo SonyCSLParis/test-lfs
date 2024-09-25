b947d9dc68d1b7d2b1940659c139b83a1009403c
Alain Riou <alain.riou14000@yahoo.com>
2024-09-25 12:07:09 +0200

    reduce size_limit to prevent 503 error

--- upload.sh
+++ upload.sh
@@ -6,15 +6,18 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # constants
-SIZE_LIMIT=4000m
+SIZE_LIMIT=2000m
 
 # pick dataset argument
 DATASET=$1
+
+# git branch $DATASET 2>/dev/null || echo "Branch $DATASET already exists"
 
 # Zip and upload
 echo "Zipping dataset $DATASET"
 zip -r -s $SIZE_LIMIT $DATASET.zip $DATASET && \
     ls $DATASET.z* > $DATASET.txt && \
+    # git checkout $DATASET && \
     git lfs track "$DATASET.z*" && \
     git add $DATASET.z* $DATASET.txt && \
     git commit -m "Added dataset $DATASET to the repo using Git LFS" && \
